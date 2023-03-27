bd997b317c015ac8f4bdb570e90fd0f7bfe8528d
Squ1ggly <squ1gglyzerocool@gmail.com>
2023-03-27 15:53:11 +1000

    FIX: Bunch of stuff

---
+++ functions/helper.js
@@ -0,0 +1,128 @@
+import fetch from "node-fetch";
+
+/**
+ * Takes an array of strings
+ * @param {string[]} strArr
+ * @returns A new array of the unique set of strings
+ */
+export function removeDuplicateValues(strArr) {
+  const foundSet = new Set();
+  for (const str of strArr) {
+    foundSet.add(str);
+  }
+  return Array.from(foundSet);
+}
+
+/**
+ * Takes an array of ip addresses
+ * @param {string[]} ipAddresses
+ * @returns An array of objects that contain the IPs and the location data assigned to it
+ */
+export async function batchFetchData(ipAddresses) {
+  const batchSize = 25;
+  const batchCount = Math.ceil(ipAddresses.length / batchSize);
+  const ipLocations = [];
+
+  for (let i = 0; i < batchCount; i++) {
+    const start = i * batchSize;
+    const end = Math.min(start + batchSize, ipAddresses.length);
+
+    const batch = ipAddresses.slice(start, end);
+
+    const res = await fetch("http://ip-api.com/batch", {
+      "Content-Type": "application/json",
+      method: "POST",
+      body: JSON.stringify(batch),
+    });
+
+    if (res.ok) {
+      const json = await res.json();
+      ipLocations.push(...json);
+      continue;
+    }
+    const resText = await res.text();
+    ipLocations.push({ error: resText });
+  }
+  return ipLocations;
+}
+
+/**
+ * This function takes an array of Microsoft Compliance Login Events
+ * @param {object[]} LoginEvents
+ * @returns An array of objects that contain the IPs and the location data assigned to it
+ */
+export async function getIpLocations(LoginEvents) {
+  const ipAddresses = LoginEvents.reduce((prev, curr) => {
+    if (!prev.includes(curr.ClientIP)) {
+      prev.push(curr.ClientIP);
+    }
+    return prev;
+  }, []);
+  return await batchFetchData(ipAddresses);
+}
+
+/**
+ * This function takes an array of Microsoft Compliance Login Events
+ * @param {object[]} LoginEvents
+ * @returns A structured array
+ */
+export function LoginEventsBreakdown(LoginEvents) {
+  const breakDown = {
+    total_number_of_logins: LoginEvents.length,
+    user_info: {},
+  };
+  for (const login of LoginEvents) {
+    if (!breakDown.user_info[login.UserId]) {
+      breakDown.user_info[login.UserId] = {
+        number_of_logins: 0,
+        login_locations: new Map(),
+      };
+    }
+
+    breakDown.user_info[login.UserId].number_of_logins += 1;
+
+    const locationKey = login.city;
+    let locationData = breakDown.user_info[login.UserId].login_locations.get(locationKey);
+    if (!locationData) {
+      locationData = {
+        country: login.country,
+        region: login.region,
+        city: login.city,
+        number_of_logins: 0,
+      };
+    }
+
+    locationData.number_of_logins += 1;
+
+    breakDown.user_info[login.UserId].login_locations.set(locationKey, locationData);
+  }
+
+  for (const userId in breakDown.user_info) {
+    const user = breakDown.user_info[userId];
+    user.login_locations = Array.from(user.login_locations.values());
+  }
+
+  return breakDown;
+}
+
+/**
+ * This function takes the output of the LoginEventsBreakdown, the function is required to to the unique shape of the data structure.
+ * @param {object[]} data
+ * @returns A specialized format CSV string.
+ */
+export function formatLoginEventsBreakdownAsCsv(data) {
+  let csv = "";
+  csv += "User Email,Number of Logins\n";
+  for (const [userEmail, user] of Object.entries(data.user_info)) {
+    const numLogins = user.number_of_logins;
+    csv += `${userEmail},${numLogins}\n`;
+    for (const location of user.login_locations) {
+      const country = location.country;
+      const region = location.region;
+      const city = location.city;
+      const numLocationLogins = location.number_of_logins;
+      csv += `,${country},${region},${city},${numLocationLogins}\n`;
+    }
+  }
+  return csv;
+}
--- index.js
+++ index.js
@@ -1,120 +1,7 @@
-const fs = require("fs"); // File system for writing files
-const fetch = require("isomorphic-fetch"); // isomorphic-fetch, cuz I'm used to it
-const converter = require("json-2-csv"); // To convert the json to csv
-
-/**
-   ______     __     ________     __                     __  _                 
-  / ____/__  / /_   /  _/ __ \   / /   ____  _________ _/ /_(_)___  ____  _____
- / / __/ _ \/ __/   / // /_/ /  / /   / __ \/ ___/ __ `/ __/ / __ \/ __ \/ ___/
-/ /_/ /  __/ /_   _/ // ____/  / /___/ /_/ / /__/ /_/ / /_/ / /_/ / / / (__  ) 
-\____/\___/\__/  /___/_/      /_____/\____/\___/\__,_/\__/_/\____/_/ /_/____/  
- */
-
-// ----------------------------------Start of helpers-----------------------------------
-
-function removeDuplicateValues(strArr) {
-  const foundSet = new Set();
-  for (const str of strArr) {
-    foundSet.add(str);
-  }
-  return Array.from(foundSet);
-}
-
-async function batchSearchIPAdd(query) {
-  const batchSize = 25;
-  const batchCount = Math.ceil(query.length / batchSize);
-  const returnArray = [];
-
-  for (let i = 0; i < batchCount; i++) {
-    const start = i * batchSize;
-    const end = Math.min(start + batchSize, query.length);
-
-    const batch = query.slice(start, end);
-
-    const res = await fetch("http://ip-api.com/batch", {
-      "Content-Type": "application/json",
-      method: "POST",
-      body: JSON.stringify(batch),
-    });
-
-    if (res.ok) {
-      const json = await res.json();
-      returnArray.push(...json);
-      continue;
-    }
-    const resText = await res.text();
-    returnArray.push({ error: resText });
-  }
-  return returnArray;
-}
-
-async function getIpLocations(loginEvents) {
-  const ips = loginEvents.map((e) => e.ClientIP);
-  const uniqueIPs = removeDuplicateValues(ips);
-
-  const querys = uniqueIPs.map((e) => {
-    return { query: e };
-  });
-
-  const removeGarbageIps = querys.filter((e) => !!e.query);
-  return await batchSearchIPAdd(removeGarbageIps);
-}
-
-function loginEventsBreakdown(LoginEvents) {
-  const returnValue = {
-    total_number_of_logins: LoginEvents.length,
-    user_info: {},
-  };
-  for (const login of LoginEvents) {
-    if (!returnValue.user_info[login.UserId]) {
-      returnValue.user_info[login.UserId] = {
-        number_of_logins: 0,
-        login_locations: new Map(),
-      };
-    }
-    returnValue.user_info[login.UserId].number_of_logins += 1;
-    const locationKey = login.city;
-    let locationData = returnValue.user_info[login.UserId].login_locations.get(locationKey);
-    if (!locationData) {
-      locationData = {
-        country: login.country,
-        region: login.region,
-        city: login.city,
-        number_of_logins: 0,
-      };
-    }
-    locationData.number_of_logins += 1;
-    returnValue.user_info[login.UserId].login_locations.set(locationKey, locationData);
-  }
-
-  for (const userId in returnValue.user_info) {
-    const user = returnValue.user_info[userId];
-    user.login_locations = Array.from(user.login_locations.values());
-  }
-
-  return returnValue;
-}
-
-function formatLoginEventsBreakdownAsCsv(data) {
-  let csv = "";
-  csv += "User Email,Number of Logins\n";
-  for (const [userEmail, user] of Object.entries(data.user_info)) {
-    const numLogins = user.number_of_logins;
-    csv += `${userEmail},${numLogins}\n`;
-    for (const location of user.login_locations) {
-      const country = location.country;
-      const region = location.region;
-      const city = location.city;
-      const numLocationLogins = location.number_of_logins;
-      csv += `,${country},${region},${city},${numLocationLogins}\n`;
-    }
-  }
-  return csv;
-}
-
-// ----------------------------------End of helpers-----------------------------------
-
-// ----------------------------------Start Main Function-----------------------------------
+import { writeFileSync } from "fs";
+import { json2csv } from "json-2-csv";
+import loginEvents from "./startingData/login_events.json" assert { type: 'json' } ;
+import { getIpLocations, LoginEventsBreakdown, formatLoginEventsBreakdownAsCsv } from './functions/helper.js';
 
 async function main(loginEvents) {
   const file = [];
@@ -141,13 +28,13 @@ async function main(loginEvents) {
     });
   }
 
-  const breakDownDataJson = loginEventsBreakdown(file);
+  const breakDownDataJson = LoginEventsBreakdown(file);
   const breakDownDataCsv = formatLoginEventsBreakdownAsCsv(breakDownDataJson);
-  const csv = await converter.json2csv(file, {});
+  const csv = await json2csv(file, {});
 
-  fs.writeFileSync("./results/login_events.csv", csv);
-  fs.writeFileSync("./results/login_events_breakdown.csv", breakDownDataCsv);
-  fs.writeFileSync("./results/login_events_breakdown.json", JSON.stringify(breakDownDataJson, null, 2));
+  writeFileSync("./results/login_events.csv", csv);
+  writeFileSync("./results/login_events_breakdown.csv", breakDownDataCsv);
+  writeFileSync("./results/login_events_breakdown.json", JSON.stringify(breakDownDataJson, null, 2));
 }
 // ----------------------------------End Main Function-----------------------------------
 
